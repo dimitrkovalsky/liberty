3ef766284bb6016d26a13a02e4a0a241c516d976
Dmytro Kovalskyi <dmytro_kovalskyi@epam.com>
2014-10-27 18:07:32 +0200

    Fixed unit tests

--- generation/src/test/scala/com/liberty/generators/RestGeneratorTest.scala
+++ generation/src/test/scala/com/liberty/generators/RestGeneratorTest.scala
@@ -8,8 +8,10 @@ import org.junit.{Assert, Test}
  */
 class RestGeneratorTest {
   @Test def generateRest() {
+    val start = System.currentTimeMillis()
     val generator = new RestGenerator()
     val result = generator.createRest(TestModels.getSubject)
+
     result match {
       case Some(rest) =>
         val expected = "package standard.rest;\n\nimport java.lang.Integer;\nimport java.lang.String;\nimport java.util.List;\nimport javax.ejb.Stateless;\nimport javax.inject.Inject;\nimport javax.ws.rs.*;\nimport javax.ws.rs.core.MediaType;\nimport standard.beans.ISubjectBean;\nimport standard.errors.ApplicationException;\nimport standard.models.Subject;\n\n@Path(\"/subjects\")\n@Stateless\npublic class SubjectResource {\n\t@Inject\n\tprivate ISubjectBean subjectBean;\n\n\t@GET\n\t@Path(\"/list\")\n\t@Produces(MediaType.APPLICATION_JSON)\n\tpublic List<Subject> getAll() throws ApplicationException {\n\t\treturn subjectBean.getAllSubjects();\n\t}\n\n\t@GET\n\t@Path(\"{id}\")\n\t@Produces(MediaType.APPLICATION_JSON)\n\tpublic Subject get(@PathParam(\"id\") Integer id) throws ApplicationException {\n\t\tSystem.out.println(\"GET get : \" + id);\n\t\treturn subjectBean.getSubject(id);\n\t}\n\n\t@POST\n\t@Path(\"/add\")\n\t@Consumes(MediaType.APPLICATION_JSON)\n\tpublic String add(Subject data) throws ApplicationException {\n\t\tSystem.out.println(\"POST add : \" + data);\n\t\tsubjectBean.addSubject(data);\n\t\treturn \"Added Subject\";\n\t}\n\n\t@POST\n\t@Path(\"/update\")\n\t@Consumes(MediaType.APPLICATION_JSON)\n\tpublic String update(Subject data) throws ApplicationException {\n\t\tSystem.out.println(\"POST update : \" + data);\n\t\tsubjectBean.updateSubject(data);\n\t\treturn \"Updated Subject\";\n\t}\n\n\t@DELETE\n\t@Path(\"{id}\")\n\tpublic String delete(@PathParam(\"id\") Integer id) {\n\t\ttry {\n\t\t\tsubjectBean.deleteSubject(id);\n\t\t\treturn \"Removed Subject with id : \" + id;\n\t\t} catch(Exception e){\n\t\t\tSystem.err.println(\"[SubjectResource] error\");\n\t\t\treturn \"Error removing Subject with id : \" + id;\n\t\t}\n\t}\n}"
